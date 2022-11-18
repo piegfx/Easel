a1b26eff5902aef64816b03793d70efe5fad892a
ollie <ollie@ollierobinson.co.uk>
2022-11-18 21:09:57 +0000

    Bitmap support to easel texture

--- Easel/Formats/EaselTexture.cs
+++ Easel/Formats/EaselTexture.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Easel.Math;
 using Easel.Utilities;
@@ -12,13 +13,50 @@ public struct EaselTexture
     
     public Size Size;
 
+    public Dictionary<BitmapLayer, Bitmap> Bitmaps;
+
     public Bitmap[] Cubemap;
 
     public EaselTexture(Bitmap[] cubemap)
     {
         Type = TextureType.Cubemap;
         Size = cubemap[0].Size;
+        foreach (Bitmap bp in cubemap)
+        {
+            if (bp.Size != Size)
+                throw new EaselException("Cubemap bitmaps must all be the same size!");
+        }
+
         Cubemap = cubemap;
+
+        Bitmaps = null;
+    }
+
+    public EaselTexture(params (BitmapLayer, Bitmap)[] bitmaps)
+    {
+        Type = TextureType.Bitmap;
+        Size = bitmaps[0].Item2.Size;
+        Bitmaps = new Dictionary<BitmapLayer, Bitmap>();
+        foreach ((BitmapLayer layer, Bitmap bp) in bitmaps)
+        {
+            if (bp.Size != Size)
+                throw new EaselException("Bitmaps must all be the same size!");
+            Bitmaps.Add(layer, bp);
+        }
+
+        Cubemap = null;
+    }
+
+    public Bitmap GetBitmap(BitmapLayer layer)
+    {
+        Bitmaps.TryGetValue(layer, out Bitmap bp);
+        return bp;
+    }
+
+    public bool TryGetBitmap(BitmapLayer layer, out Bitmap bitmap)
+    {
+        bitmap = GetBitmap(layer);
+        return bitmap != null;
     }
 
     public static EaselTexture Deserialize(byte[] data)
@@ -41,32 +79,48 @@ public struct EaselTexture
         switch (type)
         {
             case TextureType.Bitmap:
-                break;
-            
+                byte bLength = reader.ReadByte();
+                (BitmapLayer, Bitmap)[] bitmaps = new (BitmapLayer, Bitmap)[bLength];
+
+                for (int i = 0; i < bLength; i++)
+                {
+                    if (new string(reader.ReadChars(4)) != "TEXB")
+                        throw new EaselException("\"TEXB\" expected, was not found.");
+                    
+                    BitmapLayer layer = (BitmapLayer) reader.ReadByte();
+                    PixelFormat format = (PixelFormat) reader.ReadByte();
+                    
+                    Console.WriteLine(layer);
+
+                    int length = reader.ReadInt32();
+                    byte[] bData = reader.ReadBytes(length);
+                    bitmaps[i] = (layer, new Bitmap(size.Width, size.Height, format, bData));
+                }
+
+                return new EaselTexture(bitmaps);
+
             case TextureType.Cubemap:
-                PixelFormat format = (PixelFormat) reader.ReadByte();
+                PixelFormat cFmt = (PixelFormat) reader.ReadByte();
 
-                byte length = reader.ReadByte();
+                byte cLength = reader.ReadByte();
 
-                Bitmap[] bitmaps = new Bitmap[length];
+                Bitmap[] cubemaps = new Bitmap[cLength];
                 
-                for (int i = 0; i < length; i++)
+                for (int i = 0; i < cLength; i++)
                 {
                     if (new string(reader.ReadChars(4)) != "TEXC")
                         throw new EaselException("\"TEXC\" expected, was not found.");
 
                     int dataLength = reader.ReadInt32();
                     byte[] cData = reader.ReadBytes(dataLength);
-                    bitmaps[i] = new Bitmap(size.Width, size.Height, format, cData);
+                    cubemaps[i] = new Bitmap(size.Width, size.Height, cFmt, cData);
                 }
 
-                return new EaselTexture(bitmaps);
+                return new EaselTexture(cubemaps);
             
             default:
                 throw new ArgumentOutOfRangeException();
         }
-
-        return default;
     }
 
     public byte[] Serialize(bool compress)
@@ -91,7 +145,19 @@ public struct EaselTexture
         switch (Type)
         {
             case TextureType.Bitmap:
+                writer.Write((byte) Bitmaps.Count);
+                foreach ((BitmapLayer layer, Bitmap bp) in Bitmaps)
+                {
+                    writer.Write("TEXB".ToCharArray());
+                    writer.Write((byte) layer);
+                    writer.Write((byte) bp.Format);
+                    
+                    writer.Write(bp.Data.Length);
+                    writer.Write(bp.Data);
+                }
+                
                 break;
+            
             case TextureType.Cubemap:
                 writer.Write((byte) Cubemap[0].Format);
                 
@@ -108,6 +174,14 @@ public struct EaselTexture
         }
         
         return stream.ToArray();
+    }
+
+    public enum BitmapLayer : byte
+    {
+        Albedo,
+        Specular,
+        Normal,
+        Roughness
     }
 
     public enum TextureType : byte

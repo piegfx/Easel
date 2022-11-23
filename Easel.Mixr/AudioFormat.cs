6515de1160782fd2b57cd95611487b19a32c0585
ollie <ollie@ollierobinson.co.uk>
2022-11-23 17:58:00 +0000

    mixr helper functions

--- Easel.Mixr/AudioFormat.cs
+++ Easel.Mixr/AudioFormat.cs
@@ -15,4 +15,33 @@ public struct AudioFormat
         SampleRate = sampleRate;
         BitsPerSample = bitsPerSample;
     }
+
+    public AudioFormat(Pie.Audio.AudioFormat pieFormat, int sampleRate)
+    {
+        SampleRate = sampleRate;
+        
+        switch (pieFormat)
+        {
+            case Pie.Audio.AudioFormat.Mono8:
+                Channels = 1;
+                BitsPerSample = 8;
+                break;
+            case Pie.Audio.AudioFormat.Mono16:
+                Channels = 1;
+                BitsPerSample = 16;
+                break;
+            case Pie.Audio.AudioFormat.Stereo8:
+                Channels = 2;
+                BitsPerSample = 8;
+                break;
+            case Pie.Audio.AudioFormat.Stereo16:
+                Channels = 2;
+                BitsPerSample = 16;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(pieFormat), pieFormat, null);
+        }
+    }
+
+    public static readonly AudioFormat Stereo48khz = new AudioFormat(2, 48000, 16);
 }

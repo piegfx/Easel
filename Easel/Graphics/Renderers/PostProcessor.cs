0b66458a04eb60f03041a8ec45a10e44c71f2036
ollie <ollie@ollierobinson.co.uk>
2022-11-30 20:49:59 +0000

    Fixed postprocessor bug, set main target drawing and scale modes

--- Easel/Graphics/Renderers/PostProcessor.cs
+++ Easel/Graphics/Renderers/PostProcessor.cs
@@ -11,6 +11,9 @@ public class PostProcessor
     private bool _shouldAutomaticallyAdjust;
     private Size? _resolution;
 
+    public SpriteRenderMode ScalingMode;
+    public TargetDrawMode DrawMode;
+
     public Size? Resolution
     {
         get => _resolution;
@@ -32,6 +35,9 @@ public class PostProcessor
         MainTarget = new RenderTarget(resolution ?? graphics.Viewport.Size, false);
         graphics.SwapchainResized += GraphicsOnSwapchainResized;
         CreateResources(ref settings);
+
+        ScalingMode = SpriteRenderMode.Linear;
+        DrawMode = TargetDrawMode.ActualSize;
     }
 
     private void GraphicsOnSwapchainResized(Size size)
@@ -43,7 +49,7 @@ public class PostProcessor
     private void ResizeTarget(Size size)
     {
         MainTarget.Dispose();
-        MainTarget = new RenderTarget(size);
+        MainTarget = new RenderTarget(size, false);
     }
 
     public void ApplyPostProcessorSettings(ref PostProcessorSettings settings)
@@ -65,13 +71,58 @@ public class PostProcessor
 
     public void Process(EaselGraphics graphics)
     {
-        graphics.SpriteRenderer.Begin(effect: _effect);
-        graphics.SpriteRenderer.Draw(MainTarget, Vector2.Zero, null, Color.White, 0, Vector2.Zero, Vector2.One);
+        graphics.Clear(Color.Black);
+        graphics.SpriteRenderer.Begin(effect: _effect, mode: ScalingMode);
+        Vector2 position, scale, origin;
+        switch (DrawMode)
+        {
+            case TargetDrawMode.ActualSize:
+                position = Vector2.Zero;
+                scale = Vector2.One;
+                origin = Vector2.Zero;
+                break;
+            case TargetDrawMode.Stretch:
+                position = Vector2.Zero;
+                scale = new Vector2(graphics.Viewport.Width / (float) MainTarget.Size.Width,
+                    graphics.Viewport.Height / (float) MainTarget.Size.Height);
+                origin = Vector2.Zero;
+                break;
+            case TargetDrawMode.Fill:
+                scale = new Vector2(graphics.Viewport.Width > graphics.Viewport.Height
+                    ? graphics.Viewport.Height / (float) MainTarget.Size.Height
+                    : graphics.Viewport.Width / (float) MainTarget.Size.Width);
+                position = (Vector2) graphics.Viewport.Size / 2;
+                origin = (Vector2) MainTarget.Size / 2;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+        graphics.SpriteRenderer.Draw(MainTarget, position, null, Color.White, 0, origin, scale);
         graphics.SpriteRenderer.End();
     }
     
     public struct PostProcessorSettings
     {
         public bool Fxaa;
+    }
+
+    public enum TargetDrawMode
+    {
+        /// <summary>
+        /// Draw the target at its actual size, at the top left of the screen. Does not draw the target at any other
+        /// size.
+        /// </summary>
+        ActualSize,
+        
+        /// <summary>
+        /// Stretches the target to fill the entire screen.
+        /// </summary>
+        Stretch,
+        
+        /// <summary>
+        /// Fills the screen, but does not stretch.
+        /// </summary>
+        Fill
     }
 }

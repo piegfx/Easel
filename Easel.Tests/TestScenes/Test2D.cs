26a9a6ca2a10c98ce81a065fbd8b9e970331896b
ollie <ollie@ollierobinson.co.uk>
2023-01-05 18:46:56 +0000

    update test

--- Easel.Tests/TestScenes/Test2D.cs
+++ Easel.Tests/TestScenes/Test2D.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Numerics;
+using Easel.Entities;
+using Easel.Entities.Components;
 using Easel.Graphics;
 using Easel.Graphics.Renderers;
 using Easel.GUI;
 using Easel.Math;
 using Easel.Scenes;
+using Sprite = Easel.Entities.Components.Sprite;
 
 namespace Easel.Tests.TestScenes;
 
@@ -22,22 +26,23 @@ public class Test2D : Scene
         _font = Content.Load<Font>("Abel-Regular");
 
         const uint size = 128;
-        const string text = "piegfx.";
+        const string text = "docs.piegfx.com";
         
         _rt = new RenderTarget(_font.MeasureString(size, text) + new Size(0, 10));
         
         Graphics.SetRenderTarget(_rt);
+        Graphics.Clear(Color.Transparent);
         Graphics.SpriteRenderer.Begin();
-        _font.Draw(Graphics.SpriteRenderer, 128, "piegfx.", Vector2.Zero, Color.White);
+        _font.Draw(Graphics.SpriteRenderer, size, text, Vector2.Zero, Color.White);
         Graphics.SpriteRenderer.End();
         Graphics.SetRenderTarget(null);
 
         _vertices = new SpriteRenderer.SpriteVertex[]
         {
-            new(new Vector2(0, 0), new Vector2(0, 1), Color.Red),
-            new(new Vector2(_rt.Size.Width, 0), new Vector2(1, 1), Color.Green),
-            new(new Vector2(_rt.Size.Width, _rt.Size.Height), new Vector2(1, 0), Color.Blue),
-            new(new Vector2(0, _rt.Size.Height), new Vector2(0, 0), Color.White),
+            new(new Vector2(0, 0), new Vector2(0, 1), Color.Cyan),
+            new(new Vector2(_rt.Size.Width, 0), new Vector2(1, 1), Color.RebeccaPurple),
+            new(new Vector2(_rt.Size.Width, _rt.Size.Height), new Vector2(1, 0), Color.Purple),
+            new(new Vector2(0, _rt.Size.Height), new Vector2(0, 0), Color.Blue),
         };
 
         _indices = new uint[]
@@ -45,6 +50,12 @@ public class Test2D : Scene
             0u, 1u, 3u,
             1u, 2u, 3u
         };
+
+        Texture2D texture = Content.Load<Texture2D>("awesomeface");
+        
+        Entity sprite = new Entity();
+        sprite.AddComponent(new Sprite(texture));
+        Console.WriteLine(sprite.TryAddComponent(new Sprite(texture)));
     }
 
     protected override void Draw()

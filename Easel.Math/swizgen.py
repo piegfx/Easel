a7a4aa35b8b3563021e409167ad626ee91f91463
Skye <ollie@ollierobinson.co.uk>
2023-04-12 20:51:11 +0100

    update swizgen to generate arbritary length swizzles

--- Easel.Math/swizgen.py
+++ Easel.Math/swizgen.py
@@ -1,20 +1,20 @@
+def process(components, num_components, text, final_list):
+    if num_components <= 0:
+        return text;
+
+    for comp in components:
+        final_list.append(text + comp)
+        process(components, num_components - 1, text + comp, final_list)
+
 if __name__ == "__main__":
-    components = ['X', 'Y']
+    components = ['X', 'Y', 'Z', 'W']
+    max_components = 4
 
-    for comp in range(len(components)):
-        for comp2 in range(len(components)):
-            print(f"public Vector2T<T> {components[comp]}{components[comp2]} => new Vector2T<T>({components[comp]}, {components[comp2]});")
-            print()
-
-    for comp in range(len(components)):
-        for comp2 in range(len(components)):
-            for comp3 in range(len(components)):
-                print(f"public Vector3T<T> {components[comp]}{components[comp2]}{components[comp3]} => new Vector3T<T>({components[comp]}, {components[comp2]}, {components[comp3]});")
-                print()
-
-    for comp in range(len(components)):
-        for comp2 in range(len(components)):
-            for comp3 in range(len(components)):
-                for comp4 in range(len(components)):
-                    print(f"public Vector4T<T> {components[comp]}{components[comp2]}{components[comp3]}{components[comp4]} => new Vector4T<T>({components[comp]}, {components[comp2]}, {components[comp3]}, {components[comp4]});")
-                    print()
+    final_list = []
+    process(components, max_components, "", final_list)
+    final_list = sorted(final_list, key=lambda x: len(x))
+    for elem in final_list:
+        split_elem = list(elem)
+        length = len(split_elem)
+        joj = ", ".join(split_elem)
+        print(f"public Vector{length}T<T> {elem} => new Vector{length}T<T>({joj})")

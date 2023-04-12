a2787fd6922b2ecbaaf601b486a519dcbd4320dd
Skye <ollie@ollierobinson.co.uk>
2023-04-12 23:52:51 +0100

    swizgen can now output to files

--- Easel.Math/swizgen.py
+++ Easel.Math/swizgen.py
@@ -13,6 +13,10 @@ if __name__ == "__main__":
 
     struct_name = input("Enter a struct name.\nAvailable variables:\n\t\"{elem}\" - Element name\n\t\"{length}\" - The length of the element\n\t\"{params}\" - The elements in parameter form\n")
 
+    output = input("Enter an output file name (leave blank for TTY output) (WARNING THIS WILL OVERWRITE!!!). ")
+
+    pass;
+
     final_list = []
     print("Processing... (this may take a while!)")
     process(components, max_components, "", final_list)
@@ -20,9 +24,25 @@ if __name__ == "__main__":
     print("Prettyfying... (this may also take a while!)")
     final_list = sorted(final_list, key=lambda x: len(x))
 
-    for elem in final_list:
-        split_elem = list(elem)
-        length = len(split_elem)
-        joj = ", ".join(split_elem) # oops, forgot to change this. guess it's joj now
-        text = struct_name.replace("{elem}", elem).replace("{length}", str(length)).replace("{params}", joj).replace("\\n", "\n")
-        print(text)
+    # I hate the duplicate code but it was the only way I could make it "performant" (lol python moment)
+    if output == '':
+        for elem in final_list:
+            split_elem = list(elem)
+            length = len(split_elem)
+            joj = ", ".join(split_elem) # oops, forgot to change this. guess it's joj now
+            text = struct_name.replace("{elem}", elem).replace("{length}", str(length)).replace("{params}", joj).replace("\\n", "\n")
+            print(text)
+    else:
+        prompt = input(f"Writing to output file \"{output}\"?? [Y/n] ")
+        if prompt.lower() == 'y':
+            with open(output, "w") as f:
+                print("Writing... Please wait.")
+                for elem in final_list:
+                    split_elem = list(elem)
+                    length = len(split_elem)
+                    joj = ", ".join(split_elem) # oops, forgot to change this. guess it's joj now
+                    text = struct_name.replace("{elem}", elem).replace("{length}", str(length)).replace("{params}", joj).replace("\\n", "\n")
+                    f.write(text + "\n")
+            print("All done.")
+        else:
+            print("Ok.")

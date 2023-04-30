52ffdd7fdc0842c689d885d24e4821c4880b8fd0
MrScautHD <MrScautHD5@gmail.com>
2023-04-30 10:16:52 +0200

    Update InitializeSceneEventArgs.cs

--- Easel.Headless/EventArgs/InitializeSceneEventArgs.cs
+++ Easel.Headless/EventArgs/InitializeSceneEventArgs.cs
@@ -2,8 +2,8 @@ using Easel.Headless.Scenes;
 
 namespace Easel.Headless.EventArgs; 
 
-public class InitializeSceneEventArgs {
-        
+public class InitializeSceneEventArgs
+{
     public readonly Scene Scene;
 
     public InitializeSceneEventArgs(Scene scene) => Scene = scene;
